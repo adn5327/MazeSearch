dbd26686205d043072d584661dc3c80856c7611e
sa7120 <samir.agarwal@sagarwal.sagarwal>
2015-09-28 22:17:17 -0500

    updated code

--- src/pacman/infrastructure/AstarComparator.java
+++ src/pacman/infrastructure/AstarComparator.java
@@ -3,7 +3,7 @@ package pacman.infrastructure;
 import java.util.Comparator;
 
 public class AstarComparator implements Comparator<Location>{
-	
+
 
 	private Maze mazey;
 
@@ -14,6 +14,8 @@ public class AstarComparator implements Comparator<Location>{
 	public int compare(Location x, Location y){
 		int astar1 = x.getHeuristic().getDist();
 		int astar2 = y.getHeuristic().getDist();
+//		int astar1 = Heuristic.euclideanDistance(x, mazey);
+//		int astar2 = Heuristic.euclideanDistance(y, mazey);
 		return astar1 - astar2;
 	}
 }
